3b49af2ef2ad39fc54f19a9b9cebadbd3c1b48d4
ANA <ana.s.nascimento@aluno.senai.br>
2025-05-22 09:51:24 -0500

    salvar atividade de hoje

--- style.js
+++ style.js
@@ -2,3 +2,16 @@ function sim() {
     alert("Você aceitou namorar comigo!:)");
     location.href = "https://www.youtube.com/watch?v=D9W4DLjmoOM&list=RDQMd9f0yM7j-6I&index=4"
 }
+
+function desvia(btn){
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10,90);
+    btn.style.left = geraPosicao(10,90);
+    console.log('opa, desviei...');
+
+}
+
+function geraPosicao(min, max){
+    return (Math.random() * (max - min) + min) + "%"
+}
